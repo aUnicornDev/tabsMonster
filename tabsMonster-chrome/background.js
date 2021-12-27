d949644e4ca3019a83454dcb02716398ed9632ba
aUnicornDev <86775297+aUnicornDev@users.noreply.github.com>
2021-12-28 01:37:28 +0530

    CU-1xbxh3n : Updated the popup with Demo theme

--- tabsMonster-chrome/background.js
+++ tabsMonster-chrome/background.js
@@ -3,6 +3,33 @@ chrome.runtime.onInstalled.addListener(function(object) {
     chrome.tabs.create(
         { 
             url: "https://aunicorndev.github.io/tabsMonster/" 
+        },
+        ()=>{
+            getDataFromAPI("https://e5w1sl.deta.dev/monsters?theme=demo-theme-1",{})
+            .then(themes=>{
+                console.log(themes.data);
+                chrome.storage.local.set({monsterTheme: 'demo-theme-1'}, function() {
+                    
+                    });
+                chrome.storage.local.set({monsters: themes.data}, function() {
+                    
+                    });
+                }
+               
+            )
+            .catch(e=>{
+                console.log(e)
+            })
+            
         }
     );
 });
+const getDataFromAPI = async(url, body) => {
+    console.log(url)
+    const response = await fetch(url, body);
+
+    const data = await response.json();
+    return data;
+
+}
+
---
+++ tabsMonster-chrome/popup/popup.js
@@ -0,0 +1,35 @@
+const tabsCount = document.querySelector('.tabs-count');
+const monsterName = document.querySelector('.card__name');
+const monsterQuote = document.querySelector('.card__quote');
+const monsterImage = document.querySelector('.monster-image');
+const setMonsterData = (res)=>{
+    console.log(res);
+    tabsCount.textContent = res.tabsCount;
+    monsterName.textContent = res.monster.monsterName;
+    monsterQuote.textContent = res.monster.monsterQuote;
+    monsterImage.setAttribute('src',res.monster.monsterBg);
+}
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    
+    chrome.tabs.query({windowType:'normal'}, function(tabs) {    
+        chrome.storage.local.get(['monsters'], function(result) {
+            let monster;
+            let flag = 0;
+            for(let i =0;i<result.monsters.length;i++){
+                if(result.monsters[i].monsterLevel > tabs.length){
+                    monster = result.monsters[i-1]
+                    flag = 1
+                    break;
+                }
+            }
+            if(flag==0){
+                monster = result.monsters[result.monsters.length -1]
+            }
+            setMonsterData({ monster:monster, tabsCount: tabs.length })       
+        });
+    })
+    
+})
+
